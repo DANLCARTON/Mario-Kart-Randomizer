de7c86981dae896d445aa7ed3eeb5b065b9115e0
DANLCARTON <ericthiberge@orange.fr>
2023-06-14 01:45:16 +0200

    Issue #10 : suppréssion des textes pour les compteurs, ça aurait été trop galère à traduire ça

--- vues/compteur.php
+++ vues/compteur.php
@@ -1,11 +1,11 @@
 <?php
 
 function compteurCircuits() {
-    echo "<h3 id='compteur'><span id='tires'></span>/<span id='total'></span> joués. Il reste <span id='reste'></span> circuits </h3>";
+    echo "<h3 id='compteur'><span id='tires'></span>/<span id='total'></span></h3>";
 }
 
 function compteurArenes() {
-    echo "<h3 id='compteurArena'><span id='tiresArena'></span>/<span id='totalArena'></span> joués. Il reste <span id='resteArena'></span> arènes </h3>";
+    echo "<h3 id='compteurArena'><span id='tiresArena'></span>/<span id='totalArena'></span></h3>";
 }
 
 ?>
