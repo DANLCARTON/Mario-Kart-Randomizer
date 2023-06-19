13682bc00e1b12e58b3f2a8102593a2bc26259d0
DANLCARTON <ericthiberge@orange.fr>
2023-06-20 00:00:29 +0200

    #10 • Cleaning languageManager.js

--- languageManager.js
+++ languageManager.js
@@ -1,35 +1,3 @@
-/*function languageManager() {
-    var userLanguage = navigator.language;
-    var urlParams = new URLSearchParams(window.location.search);
-    var localLang = localStorage.getItem("lang");
-
-    if (localLang !== 0 && !urlParams.has("lang")) {
-        language = localLang;
-        var currentURL = window.location.href;
-        console.log("found language from local storage")
-        window.location.replace(currentURL + "?lang=" + userLanguage);
-        return language;
-    } else {
-        if (urlParams.has("lang")) {
-            var language = urlParams.get("lang");
-            console.log("found language from URL parameters")
-            return language;
-        } else {
-            if (localLang !== 0) {
-                var currentURL = window.location.href;
-                window.location.replace(currentURL + "?lang=" + localLang);
-                console.log("found language from navigator parameters")
-                return localLang;
-            } else {
-                var currentURL = window.location.href;
-                window.location.replace(currentURL + "?lang=" + userLanguage);
-                console.log("found language from navigator parameters")
-                return userLanguage;
-            }
-        }
-    }
-}*/
-
 function languageManager2() {
     var userLanguage = navigator.language;
     var urlParams = new URLSearchParams(window.location.search);
